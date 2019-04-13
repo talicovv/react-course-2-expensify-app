b8375b05e985d5362e7d0870b01e38a610977acc
talicovv <soparacadastro80@gmail.com>
2019-04-13 12:48:49 +0100

    setup formatting and createAt

--- src/test/components/ExpenseDashboardPage.test.js
+++ src/test/components/ExpenseDashboardPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
 
-test('should render DashboardPage correctly',() => {
-    const wrapper = shallow(<ExpenseDashboardPage />);
-    expect(wrapper).toMatchSnapshot();   
+test('should render ExpenseDashboardPage correctly', () => {
+  const wrapper = shallow(<ExpenseDashboardPage />);
+  expect(wrapper).toMatchSnapshot();
 });
 
